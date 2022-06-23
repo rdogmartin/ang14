b20c18a6e5e7324cda242cdca60519d5e4d4074c
Roger Martin <rmartin@aegispremier.com>
2022-06-23 16:14:52 -0500

    Update to v14 and switch to jest

---
+++ cypress/bank.fixture.ts
@@ -0,0 +1,18 @@
+import { Bank } from "src/app/shared/models/api/model";
+
+export class BankFixture {
+  private static bank1: Bank = {
+    uniqueId: 1,
+    form1UniqueId: 1,
+    name: 'Chase Bank',
+    addressLine1: '123 Four Street',
+    addressLine2: 'Unit 1001',
+    city: 'Los Angeles',
+    state: 'CA',
+    postalCode: '98554',
+  };
+
+  public static getDefault() {
+    return BankFixture.bank1;
+  }
+}
---
+++ src/app/shared/models/api/model.ts
@@ -0,0 +1,10 @@
+export interface Bank {
+  uniqueId: number;
+  form1UniqueId: number;
+  name: string;
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  state: string;
+  postalCode: string;
+}
