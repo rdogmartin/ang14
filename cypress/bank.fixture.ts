b0f77370ed3e711512d1d8d0853f73e0fa1ecd70
Roger Martin <rmartin@aegispremier.com>
2022-06-24 08:31:14 -0500

    Simplify bank model

--- cypress/bank.fixture.ts
+++ cypress/bank.fixture.ts
@@ -3,7 +3,6 @@ import { Bank } from "src/app/shared/models/api/model";
 export class BankFixture {
   private static bank1: Bank = {
     uniqueId: 1,
-    form1UniqueId: 1,
     name: 'Chase Bank',
     addressLine1: '123 Four Street',
     addressLine2: 'Unit 1001',
--- src/app/shared/models/api/model.ts
+++ src/app/shared/models/api/model.ts
@@ -1,6 +1,5 @@
 export interface Bank {
   uniqueId: number;
-  form1UniqueId: number;
   name: string;
   addressLine1: string;
   addressLine2: string;
